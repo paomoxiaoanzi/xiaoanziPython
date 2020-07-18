e9f82ad15a46a43c302645b20ca9c2eb1c7b9f01
paomoxiaoanzi <18809343303@139.com>
2020-07-18 21:18:16 +0800

    新增Python常用标准库

---
+++ Python常用标准库.py
@@ -0,0 +1,13 @@
+1、文字处理的re
+2、日期类型的time、datetime
+3、数字和数学类型的math、random
+4、文件和目录访问的pathlib、os.path
+5、数据压缩和归档的tarfile
+6、通用操作系统的os、logging、argparse
+7、多线程的threading、queue
+8、internet数据处理的base64、json、urllib
+9、结构化标记处理工具的html、xml
+10、开发工具的unitest
+11、调试工具的timeit
+12、软件包发布的venv
+13、运行服务的__main__
